a0af6a5b5f73fd4408cceb15512d2f262f5f1801
choeqq <k.chola@op.pl>
2022-10-06 17:01:53 +0200

    types

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+import { SubmitHandler, FieldValues } from 'react-hook-form';
+
+type FieldSchema = {
+  type: 'text' | 'number' | 'array' | 'object';
+};
+
+type DefaultProps = {
+  label: string;
+  placeholder?: string;
+};
+
+export type TextFieldProps = FieldSchema &
+  DefaultProps & {
+    type: 'text';
+  };
+
+export type Field = TextFieldProps;
+
+type Fields = Record<string, Field>;
+
+export interface FormProps {
+  fields: Fields;
+  onSubmit: SubmitHandler<FieldValues>;
+}
